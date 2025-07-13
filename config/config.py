02aa4b75799f11673ea65eb8a9583f453965c5fe
NishKoder <shub002gupta@gmail.com>
2025-07-13 12:48:58 +0530

    Update config/config.py

--- config/config.py
+++ config/config.py
@@ -10,8 +10,3 @@ if not GROQ_API_KEY:
 GROQ_MODEL_ID = os.getenv("GROQ_MODEL_ID")
 if not GROQ_MODEL_ID:
     raise ValueError("GROQ_MODEL_ID environment variable is not set.")
-
-# HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
-# if not HUGGINGFACEHUB_API_TOKEN:
-#     raise ValueError("HUGGINGFACEHUB_API_TOKEN environment variable is not set.")
-
